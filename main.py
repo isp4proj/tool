fcc67355a3242498598af9646402d23eae38a4a4
prizoner627 <dushyantha.world@gmail.com>
2022-12-07 00:10:46 +0530

    comments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def cis_bechmark_check():
     print(f"{Fore.BLUE}# Based on the CIS Docker Benchmark 1.3.1{Style.RESET_ALL}")
     print(f"{Fore.BLUE}# --------------------------------------------------------------------------------------------{Style.RESET_ALL}")
 
+    #executing main script
     call("./main.sh", shell=True)
 
 def finish():
@@ -23,11 +24,14 @@ def finish():
     
     logdata = {}
 
+    # get system information
     info = subprocess.run(['lsb_release', '-a'], capture_output=True, text=True).stdout
     
+    #get public ip
     ip = get('https://api.ipify.org').content.decode('utf8')
     print(f"{Fore.YELLOW}[DEBUG] Public IP address is: {ip}{Style.RESET_ALL}")
 
+    #read output file
     with open("results/output.log.json","r") as logfile:
         logdata = json.load(logfile)
 
@@ -36,19 +40,23 @@ def finish():
         "ip": str(ip),
     }
 
+    #assigning data to object
     logdata["info"] = d
 
+    #open log file to write
     with open("results/output.log.json","w") as logfile:
         print(f"{Fore.YELLOW}[DEBUG] Writing result to the log file{Style.RESET_ALL}")
         json.dump(logdata, logfile, ensure_ascii=False, indent=4)
 
+    #open log file to send result
     with open("results/output.log.json", "r") as logfile:
         data = json.loads(logfile.read())
-        # print(data)
         
+        #result server endpoint
         url = 'https://54.238.175.157:5000/results'
         print(f"{Fore.YELLOW}[DEBUG] Sending results to the server {url}{Style.RESET_ALL}")
 
+        #sending request
         r = requests.post(url, json=data,verify=False)
 
         if(r.status_code == 200):
@@ -66,11 +74,13 @@ def main():
 
     print(f"{Fore.YELLOW}[DEBUG] Cleaning previous scans \n{Style.RESET_ALL}")
 
+    #remove old result files
     if os.path.exists("./results/output.log.json"):
         os.remove("results/output.log.json")
     if os.path.exists("results/output.log"):
         os.remove("results/output.log")
 
+    #calling functions
     cis_bechmark_check()
     finish()
 
